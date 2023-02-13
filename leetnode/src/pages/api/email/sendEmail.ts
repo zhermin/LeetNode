f9616c4dbacbd56015e4a4568d94684f976a728c
Wang Mingzhe <wangmingzhe98@hotmail.com>
2023-02-13 21:37:03 +0800

    Uncomment more line to debug email api

--- leetnode/src/pages/api/email/sendEmail.ts
+++ leetnode/src/pages/api/email/sendEmail.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
+
 import { prisma } from "@/server/db/client";
-// import { Mastery } from "@prisma/client";
+import { Mastery } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // const oneWeek = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
-  // const now = new Date().getTime();
+  const oneWeek = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+  const now = new Date().getTime();
 
   //checks all mastery for ppl with errorMeter value
   const checkError = await prisma.mastery.findMany({
@@ -26,92 +27,92 @@ export default async function handler(
     },
   });
 
-  // // Filter the records to find those with errorMeter % 5,
-  // // and 5 in a row wrong and haven't been pinged in a week
-  // const usersPing = checkError.filter(
-  //   (record) =>
-  //     record.topicPing === true &&
-  //     record.errorMeter % 5 === 0 &&
-  //     (now - new Date(record.lastFlagged as Date).getTime() >= oneWeek ||
-  //       record.lastFlagged === null)
-  // );
+  // Filter the records to find those with errorMeter % 5,
+  // and 5 in a row wrong and haven't been pinged in a week
+  const usersPing = checkError.filter(
+    (record) =>
+      record.topicPing === true &&
+      record.errorMeter % 5 === 0 &&
+      (now - new Date(record.lastFlagged as Date).getTime() >= oneWeek ||
+        record.lastFlagged === null)
+  );
 
-  // console.log(usersPing);
+  console.log(usersPing);
 
-  // //get all users in prisma
-  // const allUsers = await prisma.user.findMany();
+  // get all users in prisma
+  const allUsers = await prisma.user.findMany();
 
-  // //get all emails with ADMIN role
-  // const maillist = allUsers
-  //   .filter((attribute: { role: string }) => {
-  //     return attribute.role == "ADMIN";
-  //   })
-  //   .map((admins: { email: string }) => admins.email);
+  // get all emails with ADMIN role
+  const maillist = allUsers
+    .filter((attribute: { role: string }) => {
+      return attribute.role == "ADMIN";
+    })
+    .map((admins: { email: string }) => admins.email);
 
-  // //append allUsers' names to usersPing (those ppl who will be flagged)
-  // const result: {
-  //   userId: string;
-  //   topicSlug: string;
-  //   masteryLevel: number;
-  //   topicPing: boolean;
-  //   lastFlagged: Date | null;
-  //   errorMeter: number;
-  //   topic: { topicName: string };
-  //   name: string | undefined;
-  // }[] = usersPing.map(
-  //   (ping: {
-  //     userId: string;
-  //     topicSlug: string;
-  //     masteryLevel: number;
-  //     topicPing: boolean;
-  //     lastFlagged: Date | null;
-  //     errorMeter: number;
-  //     topic: { topicName: string };
-  //   }) => {
-  //     const user = allUsers.find((u: { id: string }) => u.id === ping.userId);
-  //     return { ...ping, name: user?.name };
-  //   }
-  // );
+  // append allUsers' names to usersPing (those ppl who will be flagged)
+  const result: {
+    userId: string;
+    topicSlug: string;
+    masteryLevel: number;
+    topicPing: boolean;
+    lastFlagged: Date | null;
+    errorMeter: number;
+    topic: { topicName: string };
+    name: string | undefined;
+  }[] = usersPing.map(
+    (ping: {
+      userId: string;
+      topicSlug: string;
+      masteryLevel: number;
+      topicPing: boolean;
+      lastFlagged: Date | null;
+      errorMeter: number;
+      topic: { topicName: string };
+    }) => {
+      const user = allUsers.find((u: { id: string }) => u.id === ping.userId);
+      return { ...ping, name: user?.name };
+    }
+  );
 
-  // console.log(result);
+  console.log(result);
 
-  // // update lastFlagged when this is called
-  // result.map(async (record) => {
-  //   const flagUpdate: Mastery = await prisma.mastery.update({
-  //     where: {
-  //       userId_topicSlug: {
-  //         userId: record.userId as string,
-  //         topicSlug: record.topicSlug as string,
-  //       },
-  //     },
-  //     data: {
-  //       lastFlagged: new Date(),
-  //     },
-  //   });
-  //   console.log(flagUpdate);
-  // });
+  // update lastFlagged when this is called
+  result.map(async (record) => {
+    const flagUpdate: Mastery = await prisma.mastery.update({
+      where: {
+        userId_topicSlug: {
+          userId: record.userId as string,
+          topicSlug: record.topicSlug as string,
+        },
+      },
+      data: {
+        lastFlagged: new Date(),
+      },
+    });
+    console.log(flagUpdate);
+  });
 
   // // const usersFlagged = usersPing.map(ids => {allUsers.filter((attribute: { name: string }) => {
   // //   attribute.name === "ADMIN";
   // // })})
 
-  // const templateString = result.reduce(
-  //   (
-  //     acc: string,
-  //     rec: {
-  //       topic: { topicName: string };
-  //       name: string | undefined;
-  //       userId: string;
-  //     }
-  //   ) => {
-  //     const topicName = rec.topic.topicName;
+  const templateString = result.reduce(
+    (
+      acc: string,
+      rec: {
+        topic: { topicName: string };
+        name: string | undefined;
+        userId: string;
+      }
+    ) => {
+      const topicName = rec.topic.topicName;
 
-  //     return (
-  //       acc + `\n${topicName}:\n  Name: ${rec.name}, userId: ${rec.userId}\n`
-  //     );
-  //   },
-  //   ""
-  // );
+      return (
+        acc + `\n${topicName}:\n  Name: ${rec.name}, userId: ${rec.userId}\n`
+      );
+    },
+    ""
+  );
 
   // // Create a transporter for sending the email
   // const transporter = nodemailer.createTransport({
@@ -147,7 +148,7 @@ export default async function handler(
 
   // // res.status(200).json(templateString);
   try {
-    res.status(200).json(checkError);
+    res.status(200).json(templateString);
   } catch (err) {
     res.status(500).json({ error: err });
   }
