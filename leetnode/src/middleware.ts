26229165c9cd0b632798d7db73f4279209dcc588
Jasmine Lee <jasminelxy94@gmail.com>
2023-03-17 02:03:11 +0800

    Fix middleware

--- leetnode/src/middleware.ts
+++ leetnode/src/middleware.ts
@@ -5,10 +5,11 @@ export const config = {
     "/api/:path((?!email/sendEmail|pybkt/cronUpdate).*)",
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!_next/static|_next / image|favicon.ico).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
